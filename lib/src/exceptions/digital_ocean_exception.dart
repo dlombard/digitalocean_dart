82b8f41daec8ceb8f4ebe3ad4c425602c4854a39
dlombard <dlombard@Davensons-MacBook-Pro.local>
2020-01-05 23:52:03 -0500

    Updated exception format

--- lib/src/exceptions/digital_ocean_exception.dart
+++ lib/src/exceptions/digital_ocean_exception.dart
@@ -1,15 +1,33 @@
+import 'dart:convert';
+
 class DigitalOceanException implements Exception {
   String message;
   String id;
   int statusCode;
+  dynamic data;
+  String uri;
 
-  DigitalOceanException(this.message, this.statusCode, {this.id});
+  DigitalOceanException(this.message, this.statusCode,
+      {this.id, this.data, this.uri}) {
+    var j;
+    try {
+      j = jsonDecode(message);
+    } catch (e) {}
+    if (j != null) {
+      this.id = j['id'];
+    }
+  }
 
   @override
   String toString() {
-    String exceptionString =
-        "{id: $id, message: $message, statusCode: $statusCode}";
+    Map<String, String> errorString = {
+      "message": this.message,
+      "statusCode": this.statusCode.toString()
+    };
+    if (this.id.isNotEmpty) errorString['id'] = this.id;
+    if (this.data != null) errorString['data'] = this.data;
+    if (this.uri.isNotEmpty) errorString['uri'] = this.uri;
 
-    return exceptionString;
+    return errorString.toString();
   }
 }
